7e847bd1c1d681b1b7e7279c5aa30f2d8064f6ab
Nathan <57439772+peluche1275@users.noreply.github.com>
2020-12-10 00:36:09 +0100

    Update assurance.c

--- assurance.c
+++ assurance.c
@@ -1,65 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+short int customerAge;
+short int timeLicenceHolder;
+short int totalAccidents;
+short int numberOfContractYears;
+short int score = 0;
+char contractType[10];
+
+void userEntries()
 {
-
-    short int customerAge;
-    short int numberOfLicenceYears;
-    short int totalAccidents;
-    short int numberOfContractYears;
-    short int score = 0;
-    char contractType[10];
-
     printf("Saissisez l'âge du client :\n");
     scanf("%hd", &customerAge);
 
     printf("Saissisez le nombre d'année dont le client est titulaire du permis :\n");
-    scanf("%hd", &numberOfLicenceYears);
+    scanf("%hd", &timeLicenceHolder);
 
     printf("Saissisez le nombre d'accidents du client :\n");
     scanf("%hd", &totalAccidents);
 
     printf("Saissisez le nombre d'année dont le client est assuré :\n");
     scanf("%hd", &numberOfContractYears);
+}
 
+void scoreGeneration()
+{
     if (customerAge >= 25)
     {
         score++;
     }
 
-    if (numberOfLicenceYears >= 2)
+    if (timeLicenceHolder >= 2)
     {
         score++;
     }
 
     score = score - totalAccidents;
 
-    if (numberOfLicenceYears >= 5)
+    if (numberOfContractYears >= 5)
     {
         score++;
     }
+}
 
-    switch (score)
+void definitionOfTheContract()
+{
+
+    const char *colorsOfTheContract[] = {"Rouge", "Orange", "Vert", "Bleu"};
+
+    if (score >= 0)
     {
-    case 3:
-        strcpy(contractType, "Bleu");
-        break;
-    case 2:
-        strcpy(contractType, "Vert");
-        break;
-    case 1:
-        strcpy(contractType, "Orange");
-        break;
-    case 0:
-        strcpy(contractType, "Rouge");
-        break;
-    default:
-        strcpy(contractType, "Refusé");
-        break;
+        strcpy(contractType, colorsOfTheContract[score]);
+        printf("\n Le contrat est %s.\n", contractType);
     }
+    else
+    {
+        printf("\n Nous ne pouvons pas accepter ce client. \n");
+    }
+}
 
-    printf("\n Le contrat est %s \n", contractType);
+int main()
+{
+
+    userEntries();
+
+    scoreGeneration();
+
+    definitionOfTheContract();
 
     return 0;
 }
